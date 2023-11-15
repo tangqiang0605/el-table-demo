61f218055db3e41d34ba5ceae0bd0775afd61718
tangqiang <3256224359@qq.com>
2023-11-15 20:30:49 +0800

    抽取为组件

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,9 @@
 <template>
-  <el-table :data="tableData" style="width: 100%">
-    <el-table-column prop="a" label="aName" width="180" />
-    <el-table-column prop="b" label="bName" width="180" />
-    <el-table-column prop="c" label="cName" width="180" />
-    <el-table-column prop="d" label="dName" width="180" />
-    <el-table-column prop="e" label="eName" width="180" />
-    <el-table-column prop="f" label="fName" width="180" />
-    <el-table-column prop="g" label="gName" width="180" />
-    <el-table-column prop="h" label="hName" width="180" />
-    <el-table-column prop="i" label="iName" width="180" />
-  </el-table>
+  <CustomTable :tableData="tableData"></CustomTable>
 </template>
 
 <script lang="ts" setup>
+import CustomTable from "./CustomTable.vue";
 const tableData = new Array(9).fill({
   a: "2016-05-03",
   b: "Tom",
---
+++ src/CustomTable.vue
@@ -0,0 +1,19 @@
+<template>
+  <el-table :data="tableData" style="width: 100%">
+    <el-table-column prop="a" label="aName" width="180" />
+    <el-table-column prop="b" label="bName" width="180" />
+    <el-table-column prop="c" label="cName" width="180" />
+    <el-table-column prop="d" label="dName" width="180" />
+    <el-table-column prop="e" label="eName" width="180" />
+    <el-table-column prop="f" label="fName" width="180" />
+    <el-table-column prop="g" label="gName" width="180" />
+    <el-table-column prop="h" label="hName" width="180" />
+    <el-table-column prop="i" label="iName" width="180" />
+  </el-table>
+</template>
+
+<script lang="ts" setup>
+defineProps<{
+  tableData: Array<any>;
+}>();
+</script>
