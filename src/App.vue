82fe2654a03e6eb2346988e9e3b1e44e59899a35
tangqiang <3256224359@qq.com>
2023-11-15 20:36:35 +0800

    简单封装

--- src/App.vue
+++ src/App.vue
@@ -3,16 +3,13 @@
 </template>
 
 <script lang="ts" setup>
-import CustomTable from "./CustomTable.vue";
-const tableData = new Array(9).fill({
-  a: "2016-05-03",
-  b: "Tom",
-  c: "No. 189, Grove St, Los Angeles",
-  d: "No. 189, Grove St, Los Angeles",
-  e: "2016-05-03",
-  f: "Tom",
-  g: "No. 189, Grove St, Los Angeles",
-  h: "2016-05-03",
-  i: "Tom",
+import CustomTable from "./components/custom-table/index.vue";
+import { getData } from "./api/index";
+import { onMounted, ref } from "vue";
+const tableData = ref<any>([]);
+onMounted(() => {
+  getData().then((res: any) => {
+    tableData.value = res.data;
+  });
 });
 </script>
---
+++ src/api/index.ts
@@ -0,0 +1,20 @@
+// src/api/index.ts
+export function getData() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve({
+        data: new Array(9).fill({
+          a: "2016-05-03",
+          b: "Tom",
+          c: "No. 189, Grove St, Los Angeles",
+          d: "No. 189, Grove St, Los Angeles",
+          e: "2016-05-03",
+          f: "Tom",
+          g: "No. 189, Grove St, Los Angeles",
+          h: "2016-05-03",
+          i: "Tom",
+        }),
+      });
+    }, 200);
+  });
+}
